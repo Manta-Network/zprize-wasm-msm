b7bb7eaf45d2e2e25bc4c4c42d8d5c7c7e16f4c8
Boyuan Feng <bfeng9@wisc.edu>
2022-10-20 13:00:01 -0500

    feat: pass test for glv+BatchAffine+msm

--- wasmcurves/src/build_glv.js
+++ wasmcurves/src/build_glv.js
@@ -8,7 +8,7 @@ module.exports = function buildGLV(module, prefix, fnName) {
     const g1mField = "g1m";
 
     function toMontgomery(a) {
-        return BigInt(a) * (1n << BigInt(n8q*8)) % q;
+        return BigInt(a) * (1n << BigInt(n8q * 8)) % q;
     }
     const q = 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaabn;
 
@@ -49,12 +49,12 @@ module.exports = function buildGLV(module, prefix, fnName) {
         );
     }
 
-    // Given a pointer `pScalar` to a 256-bit scalar stored in 512-bit, decomposes into two 128-bit scalars pointed by `pScalarRes`.
+    // Given a pointer `pScalar` to a 256-bit scalar, decomposes into two 128-bit scalars pointed by `pScalarRes`.
     function buildDecomposeScalar() {
         const f = module.addFunction(fnName + "_decomposeScalar");
-        // Pointer to a 256-bit scalar stored in 512-bit memory
+        // Pointer to a 256-bit scalar
         f.addParam("pScalar", "i32");
-        // Pointer to two 128-bit scalars. These two 128-bit scalars stores the absolute value.
+        // Pointer to two 128-bit scalars. These two 128-bit scalars stores the absolute value. Each 128-bit scalar is stored in a 256-bit memory.
         f.addParam("pScalarRes", "i32");
         // Encodes the sign of two scalars. The encoding is 00...00s_1s_0 where s_0 and s_1 are the sign of k1 and k2, respectively.
         f.setReturnType("i32");
@@ -76,6 +76,10 @@ module.exports = function buildGLV(module, prefix, fnName) {
         f.addLocal("remainder", "i32");
         // Sign
         f.addLocal("sign", "i32");
+        // Stores the 256-bit pScalar in this 512-bit memory.
+        f.addLocal("pScalar512", "i32");
+        // Index
+        f.addLocal("i", "i32");
         const c = f.getCodeBuilder();
         f.addCode(
             c.setLocal("pScratchSpace", c.call(prefix + "_utility_allocateMemory", c.i32_const(64))),
@@ -84,15 +88,28 @@ module.exports = function buildGLV(module, prefix, fnName) {
             c.setLocal("pQ2", c.call(prefix + "_utility_allocateMemory", c.i32_const(64))),
             c.setLocal("pK1", c.call(prefix + "_utility_allocateMemory", c.i32_const(64))),
             c.setLocal("pK2", c.call(prefix + "_utility_allocateMemory", c.i32_const(64))),
+            c.setLocal("pScalar512", c.call(prefix + "_utility_allocateMemory", c.i32_const(64))),
+            c.call(prefix + "_int512_zero", c.getLocal("pScalar512")),
+            c.setLocal("i", c.i32_const(0)),
+            c.block(c.loop(
+                c.br_if(1, c.i32_eq(c.getLocal("i"), c.i32_const(4))),
+                c.call(prefix + "_utility_storeI64",
+                    c.getLocal("pScalar512"),
+                    c.getLocal("i"),
+                    c.call(prefix + "_utility_loadI64", c.getLocal("pScalar"), c.getLocal("i")),
+                ),
+                c.setLocal("i", c.i32_add(c.getLocal("i"), c.i32_const(1))),
+                c.br(0),
+            )),
             // q1 = (u1 * pScalar) / ((v0 * u1) - (v1 * u0));
             // Since u1 = 1, we have q1 = pScalar / ((v0 * u1) - (v1 * u0));
-            c.call(prefix + "_int512_div", c.getLocal("pScalar"), c.i32_const(pDivisor), c.getLocal("pQ1"), c.getLocal("pQr")),
+            c.call(prefix + "_int512_div", c.getLocal("pScalar512"), c.i32_const(pDivisor), c.getLocal("pQ1"), c.getLocal("pQr")),
             // q2 = (-v1 * pScalar) / ((v0 * u1) - (v1 * u0));
-            c.call(prefix + "_int512_mul", c.getLocal("pScalar"), c.i32_const(pNegV1), c.getLocal("pScratchSpace")),
+            c.call(prefix + "_int512_mul", c.getLocal("pScalar512"), c.i32_const(pNegV1), c.getLocal("pScratchSpace")),
             c.call(prefix + "_int512_div", c.getLocal("pScratchSpace"), c.i32_const(pDivisor), c.getLocal("pQ2"), c.getLocal("pQr")),
             // pK1 = pScalar - &q1 * v0 - &q2 * u0;
             // Since v0 is 1, we have pK1 = pScalar - &q1 - &q2 * u0;
-            c.drop(c.call(prefix + "_int512_sub", c.getLocal("pScalar"), c.getLocal("pQ1"), c.getLocal("pK1"))),
+            c.drop(c.call(prefix + "_int512_sub", c.getLocal("pScalar512"), c.getLocal("pQ1"), c.getLocal("pK1"))),
             c.call(prefix + "_int512_mul", c.getLocal("pQ2"), c.i32_const(pU0), c.getLocal("pScratchSpace")),
             c.drop(c.call(prefix + "_int512_sub", c.getLocal("pK1"), c.getLocal("pScratchSpace"), c.getLocal("pK1"))),
             // pK2 = 0 - q1 * v.1 - q2 * u.1;
@@ -118,10 +135,11 @@ module.exports = function buildGLV(module, prefix, fnName) {
                 c.drop(c.call(prefix + "_int512_sub", c.i32_const(pZero), c.getLocal("pK2"), c.getLocal("pK2"))),
             ),
             // pScalarRes = [pK1[0], pK1[1], pK2[0], pK2[1]]
+            c.call(prefix + "_int512_zero", c.getLocal("pScalarRes")),            
             c.call(prefix + "_utility_storeI64", c.getLocal("pScalarRes"), c.i32_const(0), c.call(prefix + "_utility_loadI64", c.getLocal("pK1"), c.i32_const(0))),
             c.call(prefix + "_utility_storeI64", c.getLocal("pScalarRes"), c.i32_const(1), c.call(prefix + "_utility_loadI64", c.getLocal("pK1"), c.i32_const(1))),
-            c.call(prefix + "_utility_storeI64", c.getLocal("pScalarRes"), c.i32_const(2), c.call(prefix + "_utility_loadI64", c.getLocal("pK2"), c.i32_const(0))),
-            c.call(prefix + "_utility_storeI64", c.getLocal("pScalarRes"), c.i32_const(3), c.call(prefix + "_utility_loadI64", c.getLocal("pK2"), c.i32_const(1))),
+            c.call(prefix + "_utility_storeI64", c.getLocal("pScalarRes"), c.i32_const(4), c.call(prefix + "_utility_loadI64", c.getLocal("pK2"), c.i32_const(0))),
+            c.call(prefix + "_utility_storeI64", c.getLocal("pScalarRes"), c.i32_const(5), c.call(prefix + "_utility_loadI64", c.getLocal("pK2"), c.i32_const(1))),
             c.i32_store(c.i32_const(0), c.getLocal("pScratchSpace")),
             c.getLocal("sign"),
         );
@@ -144,10 +162,112 @@ module.exports = function buildGLV(module, prefix, fnName) {
         );
     }
 
+    // Applies endomorphism on the input point vector `pPoints` and the input scalar vector `pScalars` of length `numPoints`.
+    // Input: each point is on affine curve, each scalar is n8q-bytes. Each point is 2*n8q-byte. 
+    //          Each scalar is n8r-byte.
+    // Output: Each point is still on affine curve, each scalar is n8q-bytes. Each point is 2*n8q-byte.
+    //          Each scalar is n8r-byte but the most significant n8r/2 bytes are 0.
+    //
+    // Input: pPoints = [p0, p1, ..., p_{n-1}]
+    //        pScalars = [s0, s1, ..., s_{n-1}]
+    // Consider pi -> pi0, pi1 
+    //          si -> si0, si1
+    // Output:
+    //        pPointsRes = [p00, p01, p10, p11, p20, p21, ..., p_{n-1}0, p_{n-1}1]
+    //        pScalarsRes = [s00, s01, s10, s11, s20, s21, ..., s_{n-1}0, s_{n-1}1]
+    function buildPreprocessEndomorphism() {
+        const f = module.addFunction(fnName + "_preprocessEndomorphism");
+        // Pointer to the input point vector. Shape: numPoints
+        f.addParam("pPoints", "i32");
+        // Pointer to the input scalar vector. Each scalar is n8r bytes. Shape: numPoints
+        f.addParam("pScalars", "i32");
+        // Number of points
+        f.addParam("numPoints", "i32");
+        // Pointer to the output point vector. Shape: 2 * numPoints
+        f.addParam("pPointsRes", "i32");
+        // Pointer to the output scalar vector. Each scalar is n8r bytes. Shape: 2 * numPoints
+        f.addParam("pScalarsRes", "i32");
+        // Index
+        f.addLocal("i", "i32");
+        // Sign
+        f.addLocal("sign", "i32");
+        const c = f.getCodeBuilder();
+        f.addCode(
+            c.setLocal("i", c.i32_const(0)),
+            c.block(c.loop(
+                c.br_if(1, c.i32_eq(c.getLocal("i"), c.getLocal("numPoints"))),
+                c.setLocal("sign",
+                    c.call(fnName + "_decomposeScalar",
+                        c.i32_add(
+                            c.getLocal("pScalars"),
+                            c.i32_mul(c.getLocal("i"), c.i32_const(n8r)),
+                        ),
+                        c.i32_add(
+                            c.getLocal("pScalarsRes"),
+                            c.i32_mul(c.getLocal("i"), c.i32_const(2*n8r)),
+                        ),
+                    ),
+                ),
+                c.call(f1mField + "_copy",
+                    c.i32_add(c.getLocal("pPoints"), c.i32_mul(c.getLocal("i"), c.i32_const(2 * n8q))),
+                    c.i32_add(c.getLocal("pPointsRes"), c.i32_mul(c.getLocal("i"), c.i32_const(4 * n8q))),
+                ),
+                c.if(c.i32_and(c.getLocal("sign"), c.i32_const(1)),
+                    c.call(f1mField + "_copy",
+                        c.i32_add(c.getLocal("pPoints"),
+                            c.i32_add(
+                                c.i32_mul(c.getLocal("i"), c.i32_const(2 * n8q)),
+                                c.i32_const(n8q),
+                            ),
+                        ),
+                        c.i32_add(c.getLocal("pPointsRes"),
+                            c.i32_add(
+                                c.i32_mul(c.getLocal("i"), c.i32_const(4 * n8q)),
+                                c.i32_const(n8q),
+                            ),
+                        ),
+                    ),
+                    c.call(f1mField + "_neg",
+                        c.i32_add(c.getLocal("pPoints"),
+                            c.i32_add(
+                                c.i32_mul(c.getLocal("i"), c.i32_const(2 * n8q)),
+                                c.i32_const(n8q),
+                            ),
+                        ),
+                        c.i32_add(c.getLocal("pPointsRes"),
+                            c.i32_add(
+                                c.i32_mul(c.getLocal("i"), c.i32_const(4 * n8q)),
+                                c.i32_const(n8q),
+                            ),
+                        ),
+                    ),
+                ),
+                c.call(fnName + "_endomorphism",
+                    c.i32_add(
+                        c.getLocal("pPoints"),
+                        c.i32_mul(c.getLocal("i"), c.i32_const(n8q * 2)),
+                    ),
+                    c.i32_shr_u(c.getLocal("sign"), c.i32_const(1)),
+                    c.i32_add(
+                        c.getLocal("pPointsRes"),
+                        c.i32_add(
+                            c.i32_mul(c.getLocal("i"), c.i32_const(4 * n8q)),
+                            c.i32_const(2 * n8q),
+                        ),
+                    ),
+                ),
+                c.setLocal("i", c.i32_add(c.getLocal("i"), c.i32_const(1))),
+                c.br(0),
+            )),
+        );
+    }
+
     buildIsPositive();
     buildDecomposeScalar();
     buildEndomorphism();
+    buildPreprocessEndomorphism();
     module.exportFunction(fnName + "_isPositive");
     module.exportFunction(fnName + "_decomposeScalar");
     module.exportFunction(fnName + "_endomorphism");
+    module.exportFunction(fnName + "_preprocessEndomorphism");
 };
